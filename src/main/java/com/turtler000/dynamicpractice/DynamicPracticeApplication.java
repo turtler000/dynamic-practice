c24f3a6dfb365ce64330873b178e4781071ff57b
hanhuanyu <hanhuanyu@inspur.com>
2020-09-11 15:39:27 +0800

    Initial commit

---
+++ src/main/java/com/turtler000/dynamicpractice/DynamicPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.turtler000.dynamicpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turtler000/dynamicpractice/DynamicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtler000.dynamicpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
